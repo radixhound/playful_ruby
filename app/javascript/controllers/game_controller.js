2f0bddfc545b9d73241bffc722a316c4efd9db21
Christopher Dwan <chris@radinine.com>
2022-07-21 09:15:55 -0700

    Add game pieces and tiles

---
+++ app/javascript/controllers/game_controller.js
@@ -0,0 +1,7 @@
+// all I need to do is add hotkeys to each of the game tiles around the player
+// then map the hotkey to the 'move to me' action?
+// this could make it easy to handle the presence of obstacles, just by
+// not rendering a 'move to me' option there?
+
+// I wonder how fast roundtrips to the server can be?
+// I wonder how much we care about it being slow or not?
