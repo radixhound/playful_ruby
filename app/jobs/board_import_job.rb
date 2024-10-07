377feff2e116f8049bdf9ba9d5210634166d9707
Christopher Dwan <chris.dwan@getdx.com>
2024-10-07 10:29:37 -0700

    Make big tiles

--- app/jobs/board_import_job.rb
+++ app/jobs/board_import_job.rb
@@ -50,6 +50,8 @@ class BoardImportJob < ApplicationJob
   def process_board(statement)
     statement.each_char.with_index do |char, index|
       tile_name = @tiles_mapping[char]
+      next if tile_name == 'empty'
+
       GameTile.create(
         game_board: @board,
         row: @row,
